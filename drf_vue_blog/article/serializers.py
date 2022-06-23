93244d5dacb093376e45d44fa8d6e017d8461b90
110% <xtmals@163.com>
2022-06-23 10:04:13 +0800

    ModelSerializer & APIView

--- drf_vue_blog/article/serializers.py
+++ drf_vue_blog/article/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
+from article.models import Article
 
-class ArticleListSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(allow_blank=True, max_length=100)
-    body = serializers.CharField(allow_blank=True)
-    created = serializers.DateTimeField()
-    updated = serializers.DateTimeField()
+# 父类变成了 ModelSerializer
+class ArticleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = [
+            'id',
+            'title',
+            'created',
+        ]
--- drf_vue_blog/article/views.py
+++ drf_vue_blog/article/views.py
@@ -3,7 +3,21 @@ from article.models import Article
 # 这个 ArticleListSerializer 暂时还没有
 from article.serializers import ArticleListSerializer
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+
+@api_view(['GET', 'POST'])
 def article_list(request):
-    articles = Article.objects.all()
-    serializer = ArticleListSerializer(articles, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ArticleListSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
